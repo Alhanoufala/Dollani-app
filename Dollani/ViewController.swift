018e38cc0298798886c460688a86f8ab704dabed
Alhanouf <79309859+Alhanoufala@users.noreply.github.com>
2022-12-09 23:55:39 +0300

    Initial Commit

---
+++ Dollani/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dollani
+//
+//  Created by Alhanouf Alawwad on 15/05/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
