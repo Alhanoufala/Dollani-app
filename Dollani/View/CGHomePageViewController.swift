b5a40a9e826cf77ed450c372f9980d5734de4a02
Layan Alwadie <layanalwadie22@gmail.com>
2022-12-15 20:03:32 +0300

    changes in home and welcome page

---
+++ Dollani/View/CGHomePageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CGHomePageViewController.swift
+//  Dollani
+//
+//  Created by Layan Alwadie on 21/05/1444 AH.
+//
+
+import UIKit
+
+class CGHomePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
