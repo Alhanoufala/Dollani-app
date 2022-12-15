133ba64b6a4b1397ffa6cc55ecf25d33bca1a61f
Layan Alwadie <layanalwadie22@gmail.com>
2022-12-16 00:15:19 +0300

    Home pages

---
+++ Dollani/View/VIContainerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VIContainerViewController.swift
+//  Dollani
+//
+//  Created by Layan Alwadie on 22/05/1444 AH.
+//
+
+import UIKit
+
+class VIContainerViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        selectedIndex = 1
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
