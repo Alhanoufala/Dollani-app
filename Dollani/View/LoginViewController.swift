b81c0f0ca3c9b571d180368213f6fb04c38acb81
Sarah AlShathri <s.i.alshathri@gmail.com>
2022-12-17 18:34:42 +0300

    direct to VI or CG homepage

--- Dollani/View/LoginViewController.swift
+++ Dollani/View/LoginViewController.swift
@@ -9,8 +9,10 @@ import UIKit
 import Firebase
 class LoginViewController: UIViewController {
     @IBOutlet weak var email: UITextField!
-    
     @IBOutlet weak var password: UITextField!
+    
+    let db = Firestore.firestore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,18 +28,28 @@ class LoginViewController: UIViewController {
         Auth.auth().signIn(withEmail: email, password: password){firebaseResult,error in
             if let e = error{
                 print(e.localizedDescription)
+                self.performSegue(withIdentifier: "GoToLoginError", sender: self)
             }
             else{
-                self.performSegue(withIdentifier: "goToHomeScreen", sender: self)
+                //check
+                let q = self.db.collection("users").whereField("email", isEqualTo: email).whereField("category", isEqualTo: "ذوي اعاقة بصرية")
+                
+                if q != nil {
+                    self.performSegue(withIdentifier: "GoToVIHomePage", sender: self)
+                }
+                else {
+                    self.performSegue(withIdentifier: "GoToCGHomePage", sender: self)}
             }
         }
     }
+    
     @IBAction func createAccountTapped(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyboard.instantiateViewController(identifier: "SignUp")
-        vc.modalPresentationStyle = .overFullScreen
-        present(vc, animated: true)
-    }
+           let storyboard = UIStoryboard(name: "Main", bundle: nil)
+           let vc = storyboard.instantiateViewController(identifier: "SignUp")
+           vc.modalPresentationStyle = .overFullScreen
+           present(vc, animated: true)
+       }
+    
     
     
     
