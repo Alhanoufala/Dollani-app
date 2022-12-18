4ce73993b574968d7045d1bb341efafab64f744b
Alhanouf <79309859+Alhanoufala@users.noreply.github.com>
2022-12-18 23:54:46 +0300

    Profile

--- Dollani/View/VIprofileViewController.swift
+++ Dollani/View/VIprofileViewController.swift
@@ -10,10 +10,51 @@ import Firebase
 
 class VIprofileViewController: UIViewController {
 
+    @IBOutlet weak var name: UITextView!
+    
+    
+    @IBOutlet weak var phoneNum: UITextView!
+    
+    @IBOutlet weak var email: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        name.textAlignment = .right
+        phoneNum.textAlignment = .right
+        email.textAlignment = .right
+        
+        retriveUserInfo()
         setupLogOutButton()
+    }
+    func retriveUserInfo(){
+        let user = Auth.auth().currentUser
+        if let user = user {
+        
+          let currentEmail = user.email
+            print(currentEmail! )
+            Firestore.firestore().collection("users").whereField("email",isEqualTo:currentEmail!).getDocuments { snapshot, error in
+                if  error != nil {
+                           // ERROR
+                       }
+                       else {
+                           if(snapshot?.count != 0){
+                            
+                               let userName = snapshot?.documents.first?.get("name") as! String
+                               let userPhoneNum = snapshot?.documents.first?.get("phoneNum") as! String
+                               let userEmail = snapshot?.documents.first?.get("email") as! String
+                               
+                               //Set textView
+                               self.name.text = userName
+                               self.phoneNum.text = userPhoneNum
+                               self.email.text = userEmail
+                           }
+                       }
+                   }
+       
+          // ...
+        }
+               
+       
     }
     
     fileprivate func setupLogOutButton() {
