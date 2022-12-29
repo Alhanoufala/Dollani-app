d7ad09f565b2d146da873866c6b40dd9d44fe21f
Layan Alwadie <layanalwadie22@gmail.com>
2022-12-30 02:50:14 +0300

    -

---
+++ Dollani/Model/Place.swift
@@ -0,0 +1,18 @@
+//
+//  Place.swift
+//  Dollani
+//
+//  Created by Layan Alwadie on 06/06/1444 AH.
+//
+
+import Foundation
+struct Place:Identifiable{
+    var id:String = UUID().uuidString
+
+    var name:String
+    var cat:String
+   
+
+    
+}
+
--- Dollani/View/placesViewController.swift
+++ Dollani/View/placesViewController.swift
@@ -10,24 +10,48 @@ import Firebase
 
 class placesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
-    var PlaceList  = [String] ()
-    var db = Firestore.firestore()
-    
+    var nameP  = ""
+    @Published var PlaceList = [Place]()
 
+    var db = Firestore.firestore()
     override func viewWillAppear(_ animated: Bool) {
-        <#code#>
+        fetchData()
+       
     }
-    override func viewDidLoad() {
+
+    func fetchData() {
+                
+        Firestore.firestore().collection("places").whereField("category", isEqualTo: "قاعات دراسية").addSnapshotListener { (querySnapshot, error) in
+            guard let documents = querySnapshot?.documents else {
+                print("No documents")
+                return
+            }
+            
+            self.PlaceList = documents.map { (queryDocumentSnapshot) -> Place in
+                let data = queryDocumentSnapshot.data()
+                let name = data["name"] as? String ?? ""
+                let cat = data["category"] as? String ?? ""
+                
+                
+                return Place(name: name, cat: cat)
+            }
+            self.tableView.reloadData()
+        }
+        
+    }
+     override func viewDidLoad() {
         super.viewDidLoad()
+         tableView.delegate = self
+         tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return PlaceList.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "placeCell", for: indexPath)
-        cell.textLabel?.text = "la"
+        cell.textLabel?.text = PlaceList[indexPath.row].name
         return cell
         
     }
