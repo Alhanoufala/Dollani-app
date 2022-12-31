4096cc080fd4944c140162ccc10b582d934447b4
Alhanouf <79309859+Alhanoufala@users.noreply.github.com>
2023-01-01 01:21:34 +0300

    -

--- Dollani/View/NavigationViewController.swift
+++ Dollani/View/NavigationViewController.swift
@@ -7,9 +7,18 @@
 
 import UIKit
 import EstimoteProximitySDK
+import CoreMotion
+import AudioToolbox
+import AVFoundation
 
 class NavigationViewController: UIViewController {
     
+    /// Provides to create an instance of the CMMotionActivityManager.
+    private let activityManager = CMMotionActivityManager()
+    /// Provides to create an instance of the CMPedometer.
+    private let pedometer = CMPedometer()
+    
+    let motionManager = CMMotionManager()
     // 1. Add a property to hold the Proximity Observer
        var proximityObserver: ProximityObserver!
     
@@ -34,18 +43,53 @@ class NavigationViewController: UIViewController {
         }
         
         self.proximityObserver.startObserving([zone])
-        // Do any additional setup after loading the view.
-    }
+        
+        activityManager.startActivityUpdates(to: OperationQueue.main) { (activity: CMMotionActivity?) in
+            guard let activity = activity else { return }
+            DispatchQueue.main.async {
+                if activity.stationary {
+                    print("Stationary")
+                } else if activity.walking {
+                    // With vibration
+                  
+                    AudioServicesPlaySystemSound(1352)
+                    print("Walking")
+                } else if activity.running {
+                    AudioServicesPlaySystemSound(1352)
+                    print("Running")
+                } else if activity.automotive {
+                    print("Automotive")
+                }
+            }
+        }
+        if CMPedometer.isStepCountingAvailable() {
+            pedometer.startUpdates(from: Date()) { pedometerData, error in
+                guard let pedometerData = pedometerData, error == nil else { return }
+                
+                DispatchQueue.main.async {
+                    print(pedometerData.numberOfSteps.intValue)
+                }
+            }
+        }
+        
+       
+        
+       // motionManager.deviceMotionUpdateInterval = 1
+       // motionManager.showsDeviceMovementDisplay = true
+      
+       // motionManager.startDeviceMotionUpdates( to:OperationQueue(), withHandler: handleUpdate)
+   
+        }
+    //func handleUpdate(data: CMDeviceMotion!, error: Error!) {
+           // if data != nil {
+               
+             //   let field = data.magneticField.field
+               
+               // print("\(field.x), \(field.y), \(field.z)")
+            //}
+    //}
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+   
+   
 }
