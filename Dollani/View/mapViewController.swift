eca0fcab3f6b26730d108dcbe30321c06fa72f86
Layan Alwadie <layanalwadie22@gmail.com>
2023-01-08 10:05:36 +0300

    map in main

---
+++ Dollani/View/mapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  mapViewController.swift
+//  Dollani
+//
+//  Created by Layan Alwadie on 15/06/1444 AH.
+//
+
+import UIKit
+
+class mapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+   
+
+}
