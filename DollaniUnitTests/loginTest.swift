5edb486f2fc574eb0bc5bf5a2b0402ec26aca382
Layan Alwadie <layanalwadie22@gmail.com>
2023-01-31 14:34:49 +0300

    unit test folder

---
+++ DollaniUnitTests/loginTest.swift
@@ -0,0 +1,15 @@
+//
+//  loginTest.swift
+//  DollaniUnitTests
+//
+//  Created by Layan Alwadie on 09/07/1444 AH.
+//
+
+//import XCTest
+//@testa
+//final class loginTest: XCTestCase {
+//
+//    var validation: LoginViewController!
+//
+//
+//}
